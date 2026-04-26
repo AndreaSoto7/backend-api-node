42128c2e7dffd066be2a51e4fdc5324eb8c2a3af
AndreaSoto7 <739501@nur.edu.bo>
2026-04-26 13:58:27 -0400

    feat: Add user authentication validation schemas

---
+++ validators/user.shema.js
@@ -0,0 +1,16 @@
+const Joi = require('joi');
+
+const registerUserSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    nombreCompleto: Joi.string().required()
+});
+
+const loginUserSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required()
+});
+
+module.exports = {
+    registerUserSchema
+};
